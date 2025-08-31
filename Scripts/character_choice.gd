36d45f6f8274be706ee05597506d1a40c9f689a0
Frisaf <frisaf07@gmail.com>
2025-08-31 12:56:15 +0200

    added start button and changed layout

--- Scripts/character_choice.gd
+++ Scripts/character_choice.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var warrior_info: RichTextLabel = $"Class Info/warrior_info"
 @onready var healer_info: RichTextLabel = $"Class Info/healer_info"
 @onready var thief_info: RichTextLabel = $"Class Info/thief_info"
+@onready var start_button: Button = $start_button
 
 var selected_class = ""
 var player_class = ""
@@ -12,15 +13,18 @@ func _on_warrior_button_pressed() -> void:
 	warrior_info.visible = true
 	healer_info.visible = false
 	thief_info.visible = false
+	start_button.visible = true
 
 func _on_healer_button_pressed() -> void:
 	selected_class = "Healer"
 	warrior_info.visible = false
 	healer_info.visible = true
 	thief_info.visible = false
+	start_button.visible = true
 
 func _on_thief_button_pressed() -> void:
 	selected_class = "Thief"
 	warrior_info.visible = false
 	healer_info.visible = false
 	thief_info.visible = true
+	start_button.visible = true
