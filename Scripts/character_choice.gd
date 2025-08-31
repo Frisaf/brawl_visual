ec59f8838f76dfd19590a8280056f74819c79104
Frisaf <frisaf07@gmail.com>
2025-08-31 12:44:32 +0200

    initial commit

---
+++ Scripts/character_choice.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var warrior_info: RichTextLabel = $"Class Info/warrior_info"
+@onready var healer_info: RichTextLabel = $"Class Info/healer_info"
+@onready var thief_info: RichTextLabel = $"Class Info/thief_info"
+
+var selected_class = ""
+var player_class = ""
+
+func _on_warrior_button_pressed() -> void:
+	selected_class = "Warrior"
+	warrior_info.visible = true
+	healer_info.visible = false
+	thief_info.visible = false
+
+func _on_healer_button_pressed() -> void:
+	selected_class = "Healer"
+	warrior_info.visible = false
+	healer_info.visible = true
+	thief_info.visible = false
+
+func _on_thief_button_pressed() -> void:
+	selected_class = "Thief"
+	warrior_info.visible = false
+	healer_info.visible = false
+	thief_info.visible = true
